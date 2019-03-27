562b98dc9d7f06fb30cfd7acfd4bb8358cff40b3
Luka7711 <35857649+Luka7711@users.noreply.github.com>
2019-03-27 13:01:07 -0500

    created aliens

--- js/app.js
+++ js/app.js
@@ -1,4 +1,69 @@
 
+
+
+// function battle() {
+
+// 	// if(Math.random() < alien.acc){
+// 	// 	ourShip.hull = ourShip.hull - fireP;
+// 	// 	console.log("ourShip has been hit");
+// 	// }
+
+// 	for(let i=ourShip.hull; i>0;i--){
+
+// 	}
+// }
+
+
+// class Ship {
+
+// 	constructor(hull, fireP, acc){
+// 		this.hull = hull;
+// 		this.fireP = fireP;
+// 		this.acc = acc;
+// 	}
+
+// 	attack(){
+
+// 	}
+// }
+
+// const ourShip = new Ship(20, 5, .7);
+
+// console.log(ourShip);
+
+
+// class alien extends Ship {
+// 	constructor(hull, fireP, acc, amountOfSships){
+// 		super(hull, fireP, acc)
+// 		this.amountOfShips = this.amountOfShips;
+// 	}
+// }
+
+
+// rndmNumGener = Math.random()*
+
+
+
+
+
+// const alien = {
+
+// 	getData(){
+	
+// 		this.hull = Math.floor(Math.random()*(6-3) + 3);
+// 		this.fireP = Math.floor(Math.random()*(4-2) + 2);
+// 		this.acc = Math.random()*(.8-.6)+.6;
+// 	}
+// }
+
+// alien.getData();
+
+// function battle() {
+
+// }
+
+
+
 const ourShip = {
 	hull:20,
 	fireP: 5,
@@ -6,12 +71,35 @@ const ourShip = {
 
 }
 
-
-const alien = {
-
-	hull: 3,
-	fireP:2,
-	acc:.6
-
+class Aliens {
+	constructor(){
+		this.hull = Math.floor(Math.random()*(6-3) + 3);
+		this.firePwr = Math.floor(Math.random()*(4-2) + 2);
+		this.acc = Math.random()*(.8-.6)+.6;
+	}
 }
+
+const a1 = new Aliens();
+const a2 = new Aliens();
+const a3 = new Aliens();
+const a4 = new Aliens();
+const a5 = new Aliens();
+const a6 = new Aliens();
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
 
